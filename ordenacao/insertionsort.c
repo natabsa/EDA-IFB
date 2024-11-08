2fd770440f37cca4adbd6f977c22fb9665778e95
Natã Bento <natabentosandrade@gmail.com>
2024-11-08 20:18:26 -0300

    modified:   ordenacao/insertionsort.c

--- ordenacao/insertionsort.c
+++ ordenacao/insertionsort.c
@@ -10,15 +10,14 @@ void insertionsortd(double *v, int n){
 		int j=i-1;
 
 		while(j>=0){
-			if(chosen<v[j]) {
+			if(v[j]>chosen) {
 				v[j+1]=v[j];
 				j--;
 			}
-			else {
-				break;
-				v[j]=chosen;
-			}
+			else break;
 		}
+
+		v[j+1]=chosen;
 
 	}
 
@@ -29,11 +28,13 @@ int main(int argc, char *argv[]){
 
 	double arr[argc-1];
 
-	for(int i=0; i<argc-1; i++) arr[i]=atof(argv[i+1]);
+	for(int i=1; i<argc; i++) arr[i-1]=atof(argv[i]);
 	
 	insertionsortd(arr, argc-1);
 
 	for(int i=0; i<argc-1; i++) printf("%.2lf, ", arr[i]);
+
+	printf("\n");
 
 	return 0;
 }
