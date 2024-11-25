cab9a935a78e3c44fe7ddf42550f5da82518e637
Natã Bento <73320913+natabsa@users.noreply.github.com>
2024-11-25 05:07:11 -0300

    Update heapsort.c

--- ordenacao/heapsort.c
+++ ordenacao/heapsort.c
@@ -1,23 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-void heapsortd(double *v, int n){
+void swapd(double *x, double *y){
 
+	double aux;
+	aux=*x;
+	*x=*y;
+	*y=aux;
+}
+/*
+void buildmaxheapd(double *heap, size_t size){
 
 
 	
 }
-*/
+*//*
+void heapfyd(double *heap, size_t size){
 
-/*
-type heapfy(){
+	if(size<2) return;
 
+	//size...
 
+	if(heap[1] > heap[0]) {
+		swapd(&heap[0], &heap[1]);
+		heapfyd(heap, size);
+	}
+	else if(heap[2] > heap[0]){
+		swapd(&heap[0], &heap[2]);
+		heapfyd(heap, size);
+	}
+}
+*//*
+void heapsortd(double *heap, size_t size){
 
+	if(size<2) return;
+
+ 	swapd(&heap[0], &heap[--size]);
+	heapfy(heap, size);
+ 	heapsortd(heap, size);
 }
 */
-
 int main(int argc, char *argv[]){
 
 	double arr[argc-1];
