9b17dfa92a19a5a12f867123d1457fcf9ca8a280
Natã Bento <natabentosandrade@gmail.com>
2024-11-26 00:03:03 -0300

    modified:   ordenacao/heapsort.c

--- ordenacao/heapsort.c
+++ ordenacao/heapsort.c
@@ -8,43 +8,48 @@ void swapd(double *x, double *y){
 	*x=*y;
 	*y=aux;
 }
-/*
-void buildmaxheapd(double *heap, size_t size){
 
+// O tipo de heap implementada e a MAX-HEAP
 
+	void heapfyd(double *heap, size_t size, size_t i){
+
+	size_t largest=i;
+
+	if((2*i+1<size)&&(heap[2*i+1] > heap[largest])) largest=2*i+1;
+	if((2*(i+1)<size)&&(heap[2*(i+1)] > heap[largest])) largest=2*(i+1);
 	
-}
-*//*
-void heapfyd(double *heap, size_t size){
-
-	if(size<2) return;
-
-	//size...
-
-	if(heap[1] > heap[0]) {
-		swapd(&heap[0], &heap[1]);
-		heapfyd(heap, size);
-	}
-	else if(heap[2] > heap[0]){
-		swapd(&heap[0], &heap[2]);
-		heapfyd(heap, size);
+	if(largest!=i){
+		swapd(&heap[i], &heap[largest]);
+		heapfyd(heap, size, largest);
 	}
 }
-*//*
+
+void buildheapd(double *arr, size_t size){
+
+	for(int i=((size-1)-1)/2; i>=0; i--) heapfyd(arr, size, i);
+}
+
 void heapsortd(double *heap, size_t size){
 
 	if(size<2) return;
 
  	swapd(&heap[0], &heap[--size]);
-	heapfy(heap, size);
+	heapfyd(heap, size, 0);
  	heapsortd(heap, size);
 }
-*/
+
 int main(int argc, char *argv[]){
 
 	double arr[argc-1];
 
 	for(int i=0; i<argc-1; i++) arr[i]=atof(argv[i+1]);
-	
+	for(int i=0; i<argc-1; i++) printf("%.2lf\n", arr[i]);
+	printf("\n");
+	buildheapd(arr, argc-1);
+	for(int i=0; i<argc-1; i++) printf("%.2lf\n", arr[i]);
+	printf("\n");
+	heapsortd(arr, argc-1);
+	for(int i=0; i<argc-1; i++) printf("%.2lf\n", arr[i]);
+
 	return 0;
 }
