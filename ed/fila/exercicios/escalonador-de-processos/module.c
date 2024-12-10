74eb5c53dbc7ad67702ae2c2e773def46207250f
Natã Bento <73320913+natabsa@users.noreply.github.com>
2024-12-10 17:18:45 -0300

    Update module.c

--- ed/fila/exercicios/escalonador-de-processos/module.c
+++ ed/fila/exercicios/escalonador-de-processos/module.c
@@ -1,17 +1,33 @@
 #include <stdlib.h>
+#include "module.c"
 
+void pqinitialize(struct pq *queue)
+{
+	queue->queue=malloc(sizeof(struct process)*4);
+	queue->front=0;
+	queue->back=0;
+	queue->capacity=4;
+	queue->size=0;
+}
 
-void pqinitialize()
+void pqexpand(struct pq *queue)
 {
 
 }
 
-void  pqdelete()
+void  pqdelete(struct pq *queue)
+{
+	free(queue->queue);
+	queue->queue=NULL;
+}
+
+void pqpush(struct pq *queue)
+{
+	if()
+}
+
+void pqpop(struct pq* queue)
 {
 
 }
 
-void pqpush()
-{
-
-}
